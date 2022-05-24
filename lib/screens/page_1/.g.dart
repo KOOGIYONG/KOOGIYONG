7adf4eb9b0d6f0f62f74e000b7f80aa0dabc6edc
Parabeac-Nest <admin@parabeac.com>
2022-05-24 16:36:40 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/screens/page_1/.g.dart
@@ -0,0 +1,61 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+
+class TempName extends StatefulWidget{
+  
+  
+  const TempName( {Key? key, }) : super(key: key);
+  @override
+  _TempName createState() => _TempName();
+}
+
+class _TempName extends State<TempName>{
+  
+  _TempName();
+
+  @override
+  Widget build(BuildContext context){
+    
+    return Material(
+color: Colors.white,
+child: Stack(
+children: [Positioned(left: 59.0,width: 99.0,top: 150.0,height: 39.0,child: LayoutBuilder( 
+  builder: (context, constraints) {
+    return Container(/** This Symbol was not found **/);
+}
+),),Positioned(left: 201.0,width: 99.0,top: 150.0,height: 39.0,child: LayoutBuilder( 
+  builder: (context, constraints) {
+    return Container(/** This Symbol was not found **/);
+}
+),),Positioned(left: 105.0,width: 109.0,top: 301.0,height: 163.0,child: Container(width: 109.000,height: 163.000,decoration: BoxDecoration(),child: Stack(
+children: [Positioned(left: 0,width: 109.0,top: 0,height: 163.0,child: Container(width: 109.000,height: 163.000,decoration: BoxDecoration(color: Color(0xffc4eccc),
+borderRadius: BorderRadius.all(Radius.circular(19)),),),),Positioned(left: 38.0,width: 34.0,top: 74.0,height: 15.0,child: Container(width: 34.000,height: 15.000,child: AutoSizeText(
+'누르기',
+style: TextStyle(
+fontFamily: 'Inter',
+fontSize: 12,
+fontWeight: FontWeight.w400,
+letterSpacing: 0,
+color: Colors.black,
+),textAlign: TextAlign.left,
+
+)),),])),),]), 
+);
+  }
+
+      @override
+    void dispose() {
+      
+      super.dispose();
+    }
+    
+}
+
